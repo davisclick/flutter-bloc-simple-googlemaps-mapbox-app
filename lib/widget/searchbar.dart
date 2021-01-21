fb599b6c23381785cca37ac82cd9455fad893032
Davis Álvarez <alvarez.davis@gmail.com>
2021-01-21 17:12:45 -0500

    Adding searchbar widget

---
+++ lib/widget/searchbar.dart
@@ -0,0 +1,12 @@
+part of 'widgets.dart';
+
+class SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 100,
+      color: Colors.red,
+    );
+  }
+}
--- lib/widget/widgets.dart
+++ lib/widget/widgets.dart
@@ -6,4 +6,5 @@ import 'package:mapa_app/bloc/my_location/my_location_bloc.dart';
 part 'btn_location.dart';
 part 'btn_my_route.dart';
 part 'btn_follow_location.dart';
+part 'searchbar.dart';
 
